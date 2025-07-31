b8cdb27f1bafd6e60a6ba28702e4f35703c5a83e
Eravity <cebotaricristian92@gmail.com>
2025-08-01 01:56:48 +0300

    Add task, reward, and event log routes to the server

--- server.ts
+++ server.ts
@@ -5,6 +5,9 @@ import { PORT } from "./config/env.ts";
 import arcjetMiddleware from "./middleware/arcjet.middleware.ts";
 import userRouter from "./routes/user.ts";
 import authRouter from "./routes/auth.ts";
+import taskRouter from "./routes/task.ts";
+import rewardRouter from "./routes/reward.ts";
+import eventLogRouter from "./routes/eventLog.ts";
 import errorMiddleware from "./middleware/error.middleware.ts";
 
 const server = express();
@@ -16,6 +19,9 @@ server.use(arcjetMiddleware);
 // server.use("/api/v1/auth", authRouter);
 server.use("/api/v1/users", userRouter);
 server.use("/api/v1/auth", authRouter);
+server.use("/api/v1/tasks", taskRouter);
+server.use("/api/v1/rewards", rewardRouter);
+server.use("/api/v1/events", eventLogRouter);
 
 server.use(errorMiddleware)
 
